3f19aa5114278c1688ce0a9e15c3c1b67ceb540c
MrBahur <MrBahur25@gmail.com>
2020-03-28 15:47:22 +0300

    removed magic numbers

--- explore.py
+++ explore.py
@@ -3,28 +3,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-PATH = 'data\\Kobi_Bryant_26_1-29_1\\US' #path to the relevant data for exploraition
+PATH = 'data\\Kobi_Bryant_26_1-29_1\\AM' #path to the relevant data for explora
 
 DATA = ('recommendation_requests_5m_rate_dc' , 
 'total_failed_action_conversions' , 
 'total_success_action_conversions' , 
 'trc_requests_timer_p95_weighted_dc' , 
 'trc_requests_timer_p99_weighted_dc')
-DATE='2020-01-26' # year-month-day
+DATE='2020-01-01' # year-month-day
 
 FILE_NAMES = [x +'_'+DATE+'.csv' for x in DATA]
-
-df = [pd.read_csv(PATH+'\\'+DATA[i]+'\\'+FILE_NAMES[i]) for i in range(0,5)]
-X = [np.array(df[i].loc[:,'ds']) for i in range(0,5)]
+df = [pd.read_csv(PATH+'\\'+DATA[i]+'\\'+FILE_NAMES[i]) for i in range(0,len(DATA))]
+X = [np.array(x.loc[:,'ds']) for x in df]
 
 arrays = [np.array(x.loc[:,'y']) for x in df]
 
-fig, axs = plt.subplots(5,1,sharex=True)
+fig, axs = plt.subplots(len(DATA),1,sharex=True)
 #Remove horisontal space between axes
 fig.subplots_adjust(hspace=0)
 
 # Plot each graph
-for i in range(0,5):
+for i in range(0,len(DATA)):
 	axs[i].plot(X[i],arrays[i])
 	axs[i].title.set_text(DATA[i])
 
