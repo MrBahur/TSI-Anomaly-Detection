f6168fc290247920eaa29bcec271e2390dfe2d20
MrBahur <MrBahur25@gmail.com>
2020-03-28 14:56:23 +0300

    fixed small bug

--- explore.py
+++ explore.py
@@ -3,19 +3,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-PATH = 'data\\Kobi_Bryant_26_1-29_1\\AM'
+PATH = 'data\\Kobi_Bryant_26_1-29_1\\US' #path to the relevant data for exploraition
 
 DATA = ('recommendation_requests_5m_rate_dc' , 
 'total_failed_action_conversions' , 
 'total_success_action_conversions' , 
 'trc_requests_timer_p95_weighted_dc' , 
 'trc_requests_timer_p99_weighted_dc')
-DATE='2020-01-01'
+DATE='2020-01-26' # year-month-day
 
 FILE_NAMES = [x +'_'+DATE+'.csv' for x in DATA]
 
 df = [pd.read_csv(PATH+'\\'+DATA[i]+'\\'+FILE_NAMES[i]) for i in range(0,5)]
-X = np.array(df[0].loc[:,'ds'])
+X = [np.array(df[i].loc[:,'ds']) for i in range(0,5)]
 
 arrays = [np.array(x.loc[:,'y']) for x in df]
 
@@ -25,7 +25,7 @@ fig.subplots_adjust(hspace=0)
 
 # Plot each graph
 for i in range(0,5):
-	axs[i].plot(X,arrays[i])
+	axs[i].plot(X[i],arrays[i])
 	axs[i].title.set_text(DATA[i])
 
 plt.show()
